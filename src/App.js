ecf95e37f1c4789e92ba9087dd136803a17c4352
Yiwen-Liu-Authright <yiwen.liu@authright.com>
2019-07-26 02:10:28 -0400

    Optimize the File Structure and the re-rendering process to save the performance

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.module.css';
-import Layout from './Components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import Burgerbuilder from './Containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
